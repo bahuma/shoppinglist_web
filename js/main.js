4ed1e096036ac68d126e84e3b113771a2c98512b
bahuma20 <bahuma20@bahuma-online.de>
2014-07-17 17:00:16 +0200

    Small changes

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@ window.addEventListener('polymer-ready', function(e) {
   
   var shoppingListApi = document.querySelector('shoppinglist-api');
   
-  shoppingListApi.loadShops(function(response) {
+  shoppingListApi.loadShops(function(e) {
     document.querySelector('template#listitem').model = {
-      response: response
+      response: e
     }
   })
   
